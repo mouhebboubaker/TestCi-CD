34ba5711f4e855bf207b43a3912363d24fe053bd
mouheb <mouhebboubaker2@gmpail.com>
2024-12-01 12:12:16 +0000

    Update

--- SourceCode/MainIndex.js
+++ SourceCode/MainIndex.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-res.send('<h1>ISIMM 24/25 : ING2 </h1>');
+res.send('<h1>ISIMM 24/25 : ING2:ING2- Semester 1 </h1>');
 });
 
 module.exports =app;
